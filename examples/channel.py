e000c4a6feffcf8e843ae092d624e8ed69c1a55c
Yusuke Tabata <tabata.yusuke@gmail.com>
2016-05-19 22:50:47 +0900

    fix channel example.

--- examples/channel.py
+++ examples/channel.py
@@ -9,6 +9,7 @@ mod = IModule(d, "mod")
 
 tab_w = ITable(mod)
 st10 = IState(tab_w)
+tab_w.initialSt = st10
 writer = design_tool.CreateChannelWrite(tab_w)
 insn_w = IInsn(writer)
 val_w = IRegister(tab_w, "w_reg")
@@ -18,6 +19,7 @@ tab_w.states.append(st10)
 
 tab_r = ITable(mod)
 st20 = IState(tab_r)
+tab_r.initialSt = st20
 reader = design_tool.CreateChannelWrite(tab_r)
 insn_r = IInsn(reader)
 val_r = IRegister(tab_r, "r_reg")
