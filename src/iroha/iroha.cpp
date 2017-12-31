f18356b10413443ec31c75d0fd704903029df3a3
Yusuke Tabata <tabata.yusuke@gmail.com>
2018-01-01 00:00:01 +0900

    Update copyright year.

--- src/iroha/iroha.cpp
+++ src/iroha/iroha.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2015-2017, Yusuke Tabata (tabata.yusuke@gmail.com) and the team.
+// Copyright (c) 2015-2018, Yusuke Tabata (tabata.yusuke@gmail.com) and the team.
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
