36848e139a4e8f82d60c38ee1064ed76d6648a83
Yusuke Tabata <tabata.yusuke@gmail.com>
2018-05-02 23:49:35 +0900

    Quick code not to make 2 ext-combinationals in a state.

--- src/opt/latency_info.cpp
+++ src/opt/latency_info.cpp
@@ -1,9 +1,15 @@
 #include "opt/latency_info.h"
 
+#include "iroha/i_design.h"
+#include "iroha/resource_class.h"
+
 namespace iroha {
 namespace opt {
 
 int LatencyInfo::GetInsnLatency(IInsn *insn) {
+  if (resource::IsExtCombinational(*(insn->GetResource()->GetClass()))) {
+    return 10;
+  }
   return 0;
 }
 
