7dbd7dbed8320be0ebc521bb433629f307ca4678
Yusuke Tabata <tabata.yusuke@gmail.com>
2018-12-24 21:21:56 +0900

    DesignWrapper class to native module.

--- examples/native.py
+++ examples/native.py
@@ -1,9 +1,10 @@
+# Please make sure using python3
 import sys
 sys.path.append('../py')
 
 from iroha.native import native
 
-d = native.Read("/tmp/a.iroha")
-w = native.CreateWriter(d)
+d = native.read("/tmp/a.iroha")
+w = native.createWriter(d)
 w.setLanguage("verilog")
 w.write("/tmp/z.v")
--- py/iroha/native/native.cpp
+++ py/iroha/native/native.cpp
@@ -1,35 +1,67 @@
+// WIP, experimental and might be deleted.
+//
+// This module provides immutable view of designs in Iroha.
+//
+// Please use python3.
+// $ make -f Build.mk
+//
+// NOTE: *Wrapper classes are used to have their own lifecycle with Python.
+
 #include <pybind11/pybind11.h>
 
 #include "iroha/iroha.h"
 
 namespace py = pybind11;
 
-int add(int i, int j) {
-  return i + j;
+class DesignWrapper {
+public:
+  DesignWrapper(iroha::IDesign *design) : design_(design) {
+  }
+
+  iroha::IDesign *GetDesign() {
+    return design_;
+  }
+
+private:
+  iroha::IDesign *design_;
+};
+
+class WriterWrapper {
+public:
+  WriterWrapper(iroha::WriterAPI *api) : api_(api) {
+  }
+
+  void SetLanguage(const string &lang) {
+    api_->SetLanguage(lang);
+  }
+
+  void Write(const string &fn) {
+    api_->Write(fn);
+  }
+
+private:
+  iroha::WriterAPI *api_;
+};
+
+DesignWrapper *ReadDesignFromFile(const string &fn) {
+  return new DesignWrapper(iroha::Iroha::ReadDesignFromFile(fn));
 }
 
-void Init() {
-  iroha::Iroha::Init();
-}
-
-iroha::IDesign *ReadDesignFromFile(const string &fn) {
-  return iroha::Iroha::ReadDesignFromFile(fn);
-}
-
-iroha::WriterAPI *CreateWriter(iroha::IDesign *design) {
-  return iroha::Iroha::CreateWriter(design);
+WriterWrapper *CreateWriter(DesignWrapper *design) {
+  return new WriterWrapper(iroha::Iroha::CreateWriter(design->GetDesign()));
 }
 
 PYBIND11_MODULE(native, m) {
   m.doc() = "Native module of Iroha";
 
-  m.def("Init", &Init, "Initialize Iroha");
-  m.def("Read", &ReadDesignFromFile, "Read a design from a file");
-  m.def("CreateWriter", &CreateWriter, "Create a writer");
+  iroha::Iroha::Init();
 
-  py::class_<iroha::IDesign>(m, "IDesign");
-  py::class_<iroha::WriterAPI>(m, "Writer")
-    .def("setLanguage", &iroha::WriterAPI::SetLanguage)
-    .def("write", &iroha::WriterAPI::Write);
+  m.def("read", &ReadDesignFromFile, "Read a design from a file");
+  m.def("createWriter", &CreateWriter, "Create a writer");
+
+  py::class_<DesignWrapper>(m, "IDesign");
+  py::class_<WriterWrapper>(m, "Writer")
+    .def("setLanguage", &WriterWrapper::SetLanguage)
+    .def("write", &WriterWrapper::Write);
   py::class_<iroha::OptAPI>(m, "Optimizer");
 }
