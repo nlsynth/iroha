55fb98883b1a0d3392d23ef13bd18906d2f45308
Yusuke Tabata <tabata.yusuke@gmail.com>
2019-03-09 14:11:40 +0900

    Add ModuleWrapper.

--- py/iroha/native/native.cpp
+++ py/iroha/native/native.cpp
@@ -8,22 +8,40 @@
 // NOTE: *Wrapper classes are used to have their own lifecycle with Python.
 
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 
 #include "iroha/iroha.h"
 
 namespace py = pybind11;
 
+class ModuleWrapper {
+public:
+  ModuleWrapper(iroha::IModule *module) : module_(module) {
+  }
+
+private:
+  iroha::IModule *module_;
+};
+
 class DesignWrapper {
 public:
   DesignWrapper(iroha::IDesign *design) : design_(design) {
+    for (auto *m : design->modules_) {
+      modules_.push_back(new ModuleWrapper(m));
+    }
   }
 
   iroha::IDesign *GetDesign() {
     return design_;
   }
 
+  vector<ModuleWrapper *> GetModules() {
+    return modules_;
+  }
+
 private:
   iroha::IDesign *design_;
+  vector<ModuleWrapper *> modules_;
 };
 
 class WriterWrapper {
@@ -59,7 +77,9 @@ PYBIND11_MODULE(native, m) {
   m.def("read", &ReadDesignFromFile, "Read a design from a file");
   m.def("createWriter", &CreateWriter, "Create a writer");
 
-  py::class_<DesignWrapper>(m, "IDesign");
+  py::class_<DesignWrapper>(m, "IDesign")
+    .def("getModules", &DesignWrapper::GetModules);
+  py::class_<ModuleWrapper>(m, "IModule");
   py::class_<WriterWrapper>(m, "Writer")
     .def("setLanguage", &WriterWrapper::SetLanguage)
     .def("write", &WriterWrapper::Write);
