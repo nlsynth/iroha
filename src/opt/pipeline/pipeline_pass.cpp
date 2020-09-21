6c1239cc912880b937357a999cdaa42f7de2a224
Yusuke Tabata <tabata.yusuke@gmail.com>
2020-09-21 23:05:28 +0900

    Don't break register vector.

--- src/opt/pipeline/pipeline_pass.cpp
+++ src/opt/pipeline/pipeline_pass.cpp
@@ -16,6 +16,7 @@ Pass *PipelinePass::Create() { return new PipelinePass(); }
 
 bool PipelinePass::ApplyForTable(const string &key, ITable *table) {
   int n = 0;
+  vector<IRegister *> loop_regs;
   for (IRegister *reg : table->registers_) {
     auto *params = reg->GetParams(false);
     if (params == nullptr) {
@@ -24,6 +25,9 @@ bool PipelinePass::ApplyForTable(const string &key, ITable *table) {
     if (!params->GetIsPipeline()) {
       continue;
     }
+    loop_regs.push_back(reg);
+  }
+  for (IRegister *reg : loop_regs) {
     loop::LoopBlock lb(table, reg);
     if (!lb.Build()) {
       continue;
