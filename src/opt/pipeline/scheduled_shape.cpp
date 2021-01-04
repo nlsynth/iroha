d85d1eb00696a355b92c248eb3cd3a8e6d496b3d
Yusuke Tabata <tabata.yusuke@gmail.com>
2021-01-04 22:52:17 +0900

    Add more details of stage shape to the comment.

--- src/opt/pipeline/scheduled_shape.cpp
+++ src/opt/pipeline/scheduled_shape.cpp
@@ -14,21 +14,22 @@ vector<pair<int, int>> &ScheduledShape::GetPipelineLocation() {
   if (locs_.size() != 0) {
     return locs_;
   }
-  // s0
-  // s0 s1
-  // s0 s1 s2
-  // .. ..
-  // s0 .. .. s{n-1}
+  // macro stage:
+  // 0:    s0              (0,0)
+  // 1:    s0 s1           (1,0),(1,1)
+  // 2:    s0 s1 s2        (2,0),(1,1),(1,2)
+  //       .. ..
+  // n-1:  s0 .. .. s{n-1} (n-1,0),(n-1,1)..(n-1,n-1)
   int ns = ssch_->GetMacroStageCount();
   for (int i = 0; i < ns; ++i) {
     for (int j = 0; j <= i; ++j) {
       locs_.push_back(make_pair(i, j));
     }
   }
-  // -- s1 s2 .. s{n-1}
-  // -- -- s2 .. s{n-1}
+  // n:    -- s1 s2 .. s{n-1}
+  //       -- -- s2 .. s{n-1}
   //             ..
-  //             s{n-1}
+  // 2n-1:             s{n-1}
   for (int i = 1; i < ns; ++i) {
     for (int j = i; j < ns; ++j) {
       locs_.push_back(make_pair(i + ns - 1, j));
