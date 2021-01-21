60ebd54c36f57ca4af503d9eb6ba27978700966a
Yusuke Tabata <tabata.yusuke@gmail.com>
2021-01-21 23:20:05 +0900

    Implement Verilog indent filter.

--- src/writer/verilog/indent.cpp
+++ src/writer/verilog/indent.cpp
@@ -1,12 +1,75 @@
 #include "writer/verilog/indent.h"
 
+#include <iostream>
+#include <sstream>
+
 namespace iroha {
 namespace writer {
 namespace verilog {
 
 Indent::Indent(const string &s) : s_(s) {}
 
-string Indent::DoIndent() { return s_; }
+string Indent::DoIndent() {
+  stringstream ss(s_);
+  string line;
+  string res;
+  int level = 0;
+  while (getline(ss, line)) {
+    line = StripSpace(line);
+    level = PreUpdateLevel(line, level);
+    res += AddIndent(line, level) + "\n";
+    level = PostUpdateLevel(line, level);
+  }
+  return res;
+}
+
+string Indent::StripSpace(const string &s) {
+  const char *c = s.c_str();
+  while (*c == ' ') {
+    ++c;
+  }
+  if (c != s.c_str()) {
+    return string(c);
+  }
+  return s;
+}
+
+string Indent::AddIndent(const string &s, int level) {
+  if (s.empty()) {
+    return s;
+  }
+  string spc = spc_[level];
+  if (spc.empty()) {
+    for (int i = 0; i < level; ++i) {
+      spc = spc + "  ";
+    }
+    spc_[level] = spc;
+  }
+  return spc + s;
+}
+
+int Indent::PreUpdateLevel(const string &line, int level) {
+  if (line.find("module") == 0 || line.find("endmodule") == 0) {
+    return 0;
+  }
+  if (line.find("end") == 0) {
+    return level - 1;
+  }
+  return level;
+}
+
+int Indent::PostUpdateLevel(const string &line, int level) {
+  if (line.find("module") == 0) {
+    return 1;
+  }
+  if (line.find("case") == 0) {
+    return level + 1;
+  }
+  if (line.find("begin") == line.size() - 5) {
+    return level + 1;
+  }
+  return level;
+}
 
 }  // namespace verilog
 }  // namespace writer
--- src/writer/verilog/indent.h
+++ src/writer/verilog/indent.h
@@ -2,7 +2,10 @@
 #ifndef _writer_verilog_indent_h_
 #define _writer_verilog_indent_h_
 
+#include <map>
+
 #include "writer/verilog/common.h"
+
 namespace iroha {
 namespace writer {
 namespace verilog {
@@ -14,7 +17,13 @@ class Indent {
   string DoIndent();
 
  private:
+  string StripSpace(const string &s);
+  string AddIndent(const string &s, int level);
+  int PreUpdateLevel(const string &line, int level);
+  int PostUpdateLevel(const string &line, int level);
+
   const string &s_;
+  map<int, string> spc_;
 };
 
 }  // namespace verilog
